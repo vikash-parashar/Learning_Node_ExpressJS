1a44895305256f99993923fe17be7cc59bf8333e
Vikash Parashar <vikash@nexg.tech>
2023-12-01 01:14:40 +0530

    making own middleware and adding to app

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
-app.use((req, res, next) => {
+
+function logger(req, res) {
     console.log("Request Method : ", req.method)
     console.log("Request URL : ", req.url)
     console.log("Response Status Code : ", res.statusCode)
-    next();
-})
+}
+app.use(logger);
 app.get("/", (req, res) => {
     res.send("Hello")
 });
